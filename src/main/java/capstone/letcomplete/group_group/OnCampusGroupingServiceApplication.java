287f678a6e342ad9a8b02414c229beb6eb03f9f9
KJungW <sagakkb0707@naver.com>
2024-03-08 18:44:28 +0900

    project start

---
+++ src/main/java/capstone/letcomplete/group_group/OnCampusGroupingServiceApplication.java
@@ -0,0 +1,13 @@
+package capstone.letcomplete.group_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCampusGroupingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnCampusGroupingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/letcomplete/group_group/OnCampusGroupingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.letcomplete.group_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnCampusGroupingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
