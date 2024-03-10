9bb75f19078dcd697f7fe57c06ea0564314de136
KJungW <sagakkb0707@naver.com>
2024-03-10 22:06:38 +0900

    Feat(setting) 추가 초기관리자 계정과 예시데이터를 삽입하는 기능
    서버를 시작할때, 초기 관리자계정을 등록하고,
    각종 예시 데이터를 삽입하는 DataSetting 스프링빈 생성

---
+++ src/main/java/capstone/letcomplete/group_group/DataSetting.java
@@ -0,0 +1,72 @@
+package capstone.letcomplete.group_group;
+
+import capstone.letcomplete.group_group.dto.input.SignupMangerInput;
+import capstone.letcomplete.group_group.dto.input.SignupMemberInput;
+import capstone.letcomplete.group_group.entity.enumtype.ManagerRoleType;
+import capstone.letcomplete.group_group.repository.ManagerRepository;
+import capstone.letcomplete.group_group.service.CampusService;
+import capstone.letcomplete.group_group.service.ManagerService;
+import capstone.letcomplete.group_group.service.MemberService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@PropertySource("classpath:application.yaml")
+public class DataSetting {
+    private final ManagerRepository managerRepository;
+    private final ManagerService managerService;
+    private final MemberService memberService;
+    private final CampusService campusService;
+    private final Environment env;
+    @PostConstruct
+    private void init() {
+        if(checkDBIsEmpty()) {
+            createManager();
+            /*
+             운영환경일 경우 제거
+             */
+            createExampleData();
+        }
+    }
+
+    private boolean checkDBIsEmpty() {
+        return managerRepository.findByEmail("manager0@back.com").isEmpty();
+    }
+
+    private void createManager() {
+        SignupMangerInput manager0 = new SignupMangerInput(
+                env.getProperty("initialData.manager.manager0.email"),
+                env.getProperty("initialData.manager.manager0.pw"),
+                "manager0", ManagerRoleType.MG_BACK);
+
+        SignupMangerInput manager1 = new SignupMangerInput(
+                env.getProperty("initialData.manager.manager1.email"),
+                env.getProperty("initialData.manager.manager1.pw"),
+                "manager1", ManagerRoleType.MG_BACK);
+
+        SignupMangerInput manager2 = new SignupMangerInput(
+                env.getProperty("initialData.manager.manager2.email"),
+                env.getProperty("initialData.manager.manager2.pw"),
+                "manager2", ManagerRoleType.MG_BACK);
+
+        SignupMangerInput manager3 = new SignupMangerInput(
+                env.getProperty("initialData.manager.manager3.email"),
+                env.getProperty("initialData.manager.manager3.pw"),
+                "manager3", ManagerRoleType.MG_BACK);
+
+        managerService.signup(manager0);
+        managerService.signup(manager1);
+        managerService.signup(manager2);
+        managerService.signup(manager3);
+    }
+
+    private void createExampleData() {
+        Long campusId = campusService.save("Hansung");
+        memberService.signup(new SignupMemberInput("test@test.com", "test1234", "Test", campusId));
+
+    }
+}
