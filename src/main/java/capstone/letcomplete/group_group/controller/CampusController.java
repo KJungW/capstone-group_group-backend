1a9f08e63a61e9bc7adc077dc88d340263aec51c
KJungW <sagakkb0707@naver.com>
2024-03-12 12:09:47 +0900

    Docs 추가 Campus컨트롤러에 대해 API문서 작성
    Campus컨트롤러에 있는 API에 대해 API문서 작성

--- src/main/java/capstone/letcomplete/group_group/controller/CampusController.java
+++ src/main/java/capstone/letcomplete/group_group/controller/CampusController.java
@@ -2,6 +2,11 @@ package capstone.letcomplete.group_group.controller;
 
 import capstone.letcomplete.group_group.dto.output.SaveCampusOutput;
 import capstone.letcomplete.group_group.service.CampusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,12 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/campus")
 @RequiredArgsConstructor
+@Tag(name="Campus", description = "Campus 관련 API")
 public class CampusController {
     private final CampusService campusService;
 
     @PostMapping()
     @PreAuthorize("hasRole('ROLE_ME_COMMON')")
+    @Operation(summary = "Add New Campus", description = "새로운 캠퍼스를 등록")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad Request(파라미터를 제대로 입력했는지 확인, 학교이름 중복등록 확인)")
+    })
     public SaveCampusOutput save(
+            @Parameter(description = "등록한 캠퍼스 이름")
             @NotBlank @RequestParam(required = true) String campusName
     ){
         return new SaveCampusOutput(campusService.save(campusName));
--- src/main/java/capstone/letcomplete/group_group/dto/output/SaveCampusOutput.java
+++ src/main/java/capstone/letcomplete/group_group/dto/output/SaveCampusOutput.java
@@ -1,5 +1,6 @@
 package capstone.letcomplete.group_group.dto.output;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,5 +8,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SaveCampusOutput {
+    @Schema(description = "등록 완료된 캠퍼스 ID")
     private Long campusId;
 }
