cb0a5813984b5245bcf4c60e179219d8e10d550e
KJungW <sagakkb0707@naver.com>
2024-03-14 17:52:54 +0900

    Fect(Exception): 추가 api호출시 발생하는 검증예외의 처리
    ExceptionGlobalControllerAdvice에서 MethodArgumentNotValidException를 처리하도록 함으로써
    api호출시 발생하는 검증예외를 처리함

--- src/main/java/capstone/letcomplete/group_group/exception/handler/ExceptionGlobalControllerAdvice.java
+++ src/main/java/capstone/letcomplete/group_group/exception/handler/ExceptionGlobalControllerAdvice.java
@@ -4,6 +4,7 @@ import capstone.letcomplete.group_group.exception.DataNotFoundException;
 import capstone.letcomplete.group_group.exception.ErrorResult;
 import capstone.letcomplete.group_group.exception.InvalidInputException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionGlobalControllerAdvice {
 
     @ResponseStatus(code= HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({DataNotFoundException.class, InvalidInputException.class})
+    @ExceptionHandler({DataNotFoundException.class, InvalidInputException.class, MethodArgumentNotValidException.class})
     public ErrorResult WrongInputExceptionHandler(Exception e) {
         return new ErrorResult("BAD_INPUT", "데이터가 존재하지 않거나 입력이 잘못되었습니다.");
     }
