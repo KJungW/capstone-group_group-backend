16346c9bceb038cb6edd6ae9b568119342d34d3b
KJungW <sagakkb0707@naver.com>
2024-03-15 22:07:34 +0900

    Feat(Exception): 추가 회원가입 관련 예외처리
    MessagingException과 SignupLogicException에 대한 예외처리 추가

--- src/main/java/capstone/letcomplete/group_group/exception/handler/ExceptionGlobalControllerAdvice.java
+++ src/main/java/capstone/letcomplete/group_group/exception/handler/ExceptionGlobalControllerAdvice.java
@@ -3,6 +3,8 @@ package capstone.letcomplete.group_group.exception.handler;
 import capstone.letcomplete.group_group.exception.DataNotFoundException;
 import capstone.letcomplete.group_group.exception.ErrorResult;
 import capstone.letcomplete.group_group.exception.InvalidInputException;
+import capstone.letcomplete.group_group.exception.SignupLogicException;
+import jakarta.mail.MessagingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,5 +18,17 @@ public class ExceptionGlobalControllerAdvice {
     @ExceptionHandler({DataNotFoundException.class, InvalidInputException.class, MethodArgumentNotValidException.class})
     public ErrorResult WrongInputExceptionHandler(Exception e) {
         return new ErrorResult("BAD_INPUT", "데이터가 존재하지 않거나 입력이 잘못되었습니다.");
+    }
+
+    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MessagingException.class)
+    public ErrorResult WrongInputExceptionHandler(MessagingException e) {
+        return new ErrorResult("MAIL_SEND_ERROR", "데이터가 존재하지 않거나 입력이 잘못되었습니다.");
+    }
+
+    @ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(SignupLogicException.class)
+    public ErrorResult WrongInputExceptionHandler(SignupLogicException e) {
+        return new ErrorResult("SIGNUP_LOGIC_ERROR", "회원가입 중 예상치 못한 에러발생");
     }
 }
