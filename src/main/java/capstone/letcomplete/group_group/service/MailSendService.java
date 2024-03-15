930a42de7b94d51fed2fcbe2a6ffb012134fd6e3
KJungW <sagakkb0707@naver.com>
2024-03-15 17:48:40 +0900

    Feat(Mail): 추가 MailSendService
    메일 전송을 담당하는 MailSendService 추가

---
+++ src/main/java/capstone/letcomplete/group_group/service/MailSendService.java
@@ -0,0 +1,29 @@
+package capstone.letcomplete.group_group.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class MailSendService {
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String email, String title, String content) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setTo(email);
+        helper.setSubject(title);
+        helper.setText(content);
+        mailSender.send(mimeMessage);
+    }
+
+
+}
