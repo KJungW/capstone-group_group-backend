3edc810ad175968cc081aae2e14b38434359833a
KJungW <sagakkb0707@naver.com>
2024-03-15 22:19:13 +0900

    Docs: 변경 회원가입 API 문서
    회원가입에 이메일 인증이 추가됨에 따라 API문서 변경

--- src/main/java/capstone/letcomplete/group_group/controller/MemberController.java
+++ src/main/java/capstone/letcomplete/group_group/controller/MemberController.java
@@ -4,8 +4,6 @@ import capstone.letcomplete.group_group.dto.input.SignupMemberInput;
 import capstone.letcomplete.group_group.dto.output.SignupOutput;
 import capstone.letcomplete.group_group.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
@@ -20,24 +18,18 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    @Operation(summary = "Create New Member", description = "일반 회원(Member)에 대한 회원가입")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request(파라미터 유효성, 이메일 중복, 캠퍼스ID 유효성 등을 확인")
-    })
+    @Operation(summary = "Signup Start", description = "일반 회원(Member)에 대한 회원가입을 인증메일을 요청하는 API")
     public void signupStart(@RequestBody @Valid SignupMemberInput input) throws MessagingException {
         memberService.signupStart(input);
     }
 
     @GetMapping("signup/complete")
+    @Operation(summary = "Signup Complete", description = "회원가입 인증메일을 클릭했을때 실행되는 API")
     public SignupOutput signupComplete(
             @RequestParam(name="email") String email,
             @RequestParam(name="certificationNumber") String certificationNumber
     )  {
         return new SignupOutput(memberService.signupComplete(email, certificationNumber));
     }
-
-
-
 
 }
