3ef5389097b041e5feabeb8d51f1490f608d0005
KJungW <sagakkb0707@naver.com>
2024-03-16 21:46:31 +0900

    Feat(Auth): 변경 인증과 인가에 대한 예외처리 핸들러에서의 json 변환방식
    JwtAccessDeniedHandler와 JwtAuthenticationEntryPoint에서 예외내용을 json으로 변경할때,
    ObjectMapper가 아닌 JsonUtil 스프링빈을 활용하도록 변경

--- src/main/java/capstone/letcomplete/group_group/exception/handler/JwtAccessDeniedHandler.java
+++ src/main/java/capstone/letcomplete/group_group/exception/handler/JwtAccessDeniedHandler.java
@@ -1,10 +1,12 @@
 package capstone.letcomplete.group_group.exception.handler;
 
 import capstone.letcomplete.group_group.exception.ErrorResult;
+import capstone.letcomplete.group_group.util.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,16 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final JsonUtil jsonUtil;
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
 
-        String content = objectMapper.writeValueAsString(
+        String content = jsonUtil.convertObjectToJson(
                 new ErrorResult("FORBIDDEN","권한이 없습니다."));
 
         response.getWriter().write(content);
--- src/main/java/capstone/letcomplete/group_group/exception/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/capstone/letcomplete/group_group/exception/handler/JwtAuthenticationEntryPoint.java
@@ -1,10 +1,12 @@
 package capstone.letcomplete.group_group.exception.handler;
 
 import capstone.letcomplete.group_group.exception.ErrorResult;
+import capstone.letcomplete.group_group.util.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,16 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final JsonUtil jsonUtil;
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
 
-        String content = objectMapper.writeValueAsString(
+        String content = jsonUtil.convertObjectToJson(
                 new ErrorResult("UNAUTHORIZED","계정정보가 올바르지 않습니다. 다시 로그인을 해주세요"));
 
         response.getWriter().write(content);
