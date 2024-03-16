8ed463e62b333dcad18f3eec25fb34ab766c6767
KJungW <sagakkb0707@naver.com>
2024-03-16 21:55:39 +0900

    Feat(Board): 추가 Board엔티티에 대한 레파지토리
    Board 엔티티에 대한 레파지토리 생성

---
+++ src/main/java/capstone/letcomplete/group_group/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package capstone.letcomplete.group_group.repository;
+
+import capstone.letcomplete.group_group.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
