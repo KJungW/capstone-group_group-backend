34afaaa1f504d8d1c97d9ec3df7e6c7580ba57ba
KJungW <sagakkb0707@naver.com>
2024-03-16 22:11:51 +0900

    Feat(Board): 추가 BoardService
    Board 관련 비즈니스 로직을 처리하는 BoardService 생성
    createBoard() 메서드를 통해 게시판 추가
    findById() 메서드를 통해 ID를 활용한 게시판 조회 구현

---
+++ src/main/java/capstone/letcomplete/group_group/service/BoardService.java
@@ -0,0 +1,31 @@
+package capstone.letcomplete.group_group.service;
+
+import capstone.letcomplete.group_group.entity.Board;
+import capstone.letcomplete.group_group.entity.Campus;
+import capstone.letcomplete.group_group.exception.DataNotFoundException;
+import capstone.letcomplete.group_group.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+    private final CampusService campusService;
+
+    @Transactional
+    public Long createBoard(Long campusId, String title) {
+        Campus campus = campusService.findById(campusId);
+        Board newBoard = Board.makeBoard(campus, title);
+        boardRepository.save(newBoard);
+        return newBoard.getId();
+    }
+
+    public Board findById(Long id) {
+        return boardRepository.findById(id).orElseThrow(
+                ()->new DataNotFoundException("ID에 해당하는 게시판이 존재하지 않습니다.")
+        );
+    }
+}
