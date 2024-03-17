a07d8b20b5ded3c31a5430da7cedf8a0984ebd99
KJungW <sagakkb0707@naver.com>
2024-03-17 12:50:15 +0900

    Feat(RequirementsForm): 추가 RequirementsForm엔티티에 대한 레파지토리
    RequirementsForm엔티티에 대한 레파지토리인 RequirementsFormRepository 추가

---
+++ src/main/java/capstone/letcomplete/group_group/repository/RequirementsFormRepository.java
@@ -0,0 +1,8 @@
+package capstone.letcomplete.group_group.repository;
+
+import capstone.letcomplete.group_group.entity.RequirementsForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequirementsFormRepository extends JpaRepository<RequirementsForm, Long> {
+}
+
