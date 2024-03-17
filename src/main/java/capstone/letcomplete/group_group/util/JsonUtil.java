cf2683d97aa31c587f08a03e15f95b8ef7ac6784
KJungW <sagakkb0707@naver.com>
2024-03-17 19:42:58 +0900

    Feat(Util): 추가 josn을 리스트형태의 데이터로 변환하는 로직
    JsonUtil에 convertJsonToList()를 통해 json데이터를 리스트형태의 데이터로 변환

--- src/main/java/capstone/letcomplete/group_group/util/JsonUtil.java
+++ src/main/java/capstone/letcomplete/group_group/util/JsonUtil.java
@@ -1,8 +1,11 @@
 package capstone.letcomplete.group_group.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class JsonUtil {
@@ -14,5 +17,9 @@ public class JsonUtil {
 
     public  <T> T convertJsonToObject(String json,  Class<T> clazz) throws JsonProcessingException {
         return objectMapper.readValue(json, clazz);
+    }
+
+    public <T> List<T> convertJsonToList(String json, Class<T> clazz) throws JsonProcessingException {
+        return objectMapper.readValue(json, new TypeReference<List<T>>() {});
     }
 }
