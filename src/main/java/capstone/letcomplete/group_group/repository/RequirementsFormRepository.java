f22261060fd03dabb91fd8e9294613bc432a4b65
KJungW <sagakkb0707@naver.com>
2024-03-17 18:28:41 +0900

    Feat(Form): 추가 findNewestWithPostId() 서비스 로직
    같은 Post ID를 가지는 RequirementsForm중에 가장 최근에 만들어진 RequirementsForm을 조회

--- src/main/java/capstone/letcomplete/group_group/repository/RequirementsFormRepository.java
+++ src/main/java/capstone/letcomplete/group_group/repository/RequirementsFormRepository.java
@@ -1,8 +1,17 @@
 package capstone.letcomplete.group_group.repository;
 
 import capstone.letcomplete.group_group.entity.RequirementsForm;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RequirementsFormRepository extends JpaRepository<RequirementsForm, Long> {
+    @Query( "select f from RequirementsForm f where f.post.id = :postId")
+    List<RequirementsForm> findByPostId(@Param("postId") Long id, Pageable pageable);
+
+
 }
 
--- src/main/java/capstone/letcomplete/group_group/service/RequirementsFormService.java
+++ src/main/java/capstone/letcomplete/group_group/service/RequirementsFormService.java
@@ -5,6 +5,8 @@ import capstone.letcomplete.group_group.exception.DataNotFoundException;
 import capstone.letcomplete.group_group.repository.RequirementsFormRepository;
 import capstone.letcomplete.group_group.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +29,14 @@ public class RequirementsFormService {
         return requirementsFormRepository.findById(id).orElseThrow(
                 ()-> new DataNotFoundException("ID에 해당하는 Post가 존재하지 않습니다.")
         );
+    }
+
+    public RequirementsForm findNewestWithPostId(Long id) {
+        PageRequest pageRequest = PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "createDate"));
+        List<RequirementsForm> forms = requirementsFormRepository.findByPostId(id, pageRequest);
+        if(forms.isEmpty())
+            throw new DataNotFoundException("id에 해당하는 RequirementsForm이 존재하지 않습니다.");
+        return forms.get(0);
     }
 
 }
