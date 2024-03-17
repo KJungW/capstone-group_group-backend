d98a9feb7d9fc8358f0627eb32eb9ffa3432f98b
KJungW <sagakkb0707@naver.com>
2024-03-17 20:28:11 +0900

    Feat(Post): 추가 모집글 세부정보 조회 API
    모집글에 대한 세부정로를 조회하는 PostController의 GetPostDetailOutput() API 생성

--- src/main/java/capstone/letcomplete/group_group/controller/PostController.java
+++ src/main/java/capstone/letcomplete/group_group/controller/PostController.java
@@ -1,15 +1,13 @@
 package capstone.letcomplete.group_group.controller;
 
 import capstone.letcomplete.group_group.dto.input.CreatePostInput;
+import capstone.letcomplete.group_group.dto.output.GetPostDetailOutput;
 import capstone.letcomplete.group_group.dto.output.SavePostOutput;
 import capstone.letcomplete.group_group.service.PostUsageService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
@@ -21,5 +19,10 @@ public class PostController {
     public SavePostOutput savePost(@Valid @RequestBody CreatePostInput createPostInput) throws JsonProcessingException {
         Long savedPostId = postUsageService.savePost(createPostInput);
         return new SavePostOutput(savedPostId);
+    }
+
+    @GetMapping
+    public GetPostDetailOutput getPostDetail(@RequestParam(name = "postId") Long id) throws JsonProcessingException {
+        return postUsageService.getPostDetail(id);
     }
 }
