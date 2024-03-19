750ec00fb3f492317a75bc0240679c48cdd83e43
KJungW <sagakkb0707@naver.com>
2024-03-19 10:50:54 +0900

    Feat(Application): 추가 Application을 위한 레파지토리
    Application을 위한 레파지토리인 ApplicationRepository 추가

---
+++ src/main/java/capstone/letcomplete/group_group/repository/ApplicationRepository.java
@@ -0,0 +1,7 @@
+package capstone.letcomplete.group_group.repository;
+
+import capstone.letcomplete.group_group.entity.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
