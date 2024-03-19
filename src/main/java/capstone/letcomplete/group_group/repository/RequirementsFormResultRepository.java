f58ac6135499707d55976680f51ad2762922a9e7
KJungW <sagakkb0707@naver.com>
2024-03-19 10:45:00 +0900

    feat(RequirementsFormResult): 추가 RequirementsFormResult를 위한 레파지토리
    RequirementsFormResult를 위한 레파지토리인 RequirementsFormResultRepository 생성

---
+++ src/main/java/capstone/letcomplete/group_group/repository/RequirementsFormResultRepository.java
@@ -0,0 +1,7 @@
+package capstone.letcomplete.group_group.repository;
+
+import capstone.letcomplete.group_group.entity.RequirementsFormResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequirementsFormResultRepository extends JpaRepository<RequirementsFormResult, Long> {
+}
