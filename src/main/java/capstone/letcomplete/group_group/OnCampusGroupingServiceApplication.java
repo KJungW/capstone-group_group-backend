377f9094fa91770b3955b941d0c1697a7d3b2328
KJungW <sagakkb0707@naver.com>
2024-05-23 21:29:53 +0900

    Feat(TimeZone): 추가 타임존 세팅
    현재 프로젝트의 타임존을 "Asia/Seoul"로 변경하는 setTimeZone()코드 추가

--- src/main/java/capstone/letcomplete/group_group/OnCampusGroupingServiceApplication.java
+++ src/main/java/capstone/letcomplete/group_group/OnCampusGroupingServiceApplication.java
@@ -1,8 +1,11 @@
 package capstone.letcomplete.group_group;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class OnCampusGroupingServiceApplication {
 		SpringApplication.run(OnCampusGroupingServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
